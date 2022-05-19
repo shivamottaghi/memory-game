0ce3de7dce5cf7ecf0357df5b937d8d9b0c85795
shivamtg <shivamtg1376@gmail.com>
2022-05-19 12:31:03 +0200

    add array with img urls

---
+++ script.js
@@ -0,0 +1,14 @@
+
+(()=>{
+    let imgUrlArr = [
+        'images/ice-cream.png'
+        ,'images/palm-tree.png'
+        ,'images/photo-camera.png'
+        ,'images/pineapple.png'
+        ,'images/sun.png'
+        ,'images/sunglasses.png'
+        ,'images/umbrella.png'
+        ,'images/watermelon.png'
+    ];
+
+})();
