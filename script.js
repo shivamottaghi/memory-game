d7ff77393a112a4a4efd984cf58f666cd9edeb03
shivamtg <shivamtg1376@gmail.com>
2022-05-19 15:15:39 +0200

    display img in random order when btn clicked

--- script.js
+++ script.js
@@ -10,15 +10,21 @@
         ,'images/umbrella.png'
         ,'images/watermelon.png'
     ];
-    let imgDoubArr = imgUrlArr.concat(imgUrlArr)
-    console.log('original' + imgDoubArr);
+    let imgDoubArr = imgUrlArr.concat(imgUrlArr);
+
 
     //___________________________START___________________________
     document.getElementById('start').addEventListener('click', ()=>{
+        let imgTags = document.querySelectorAll('img');
+        imgTags.forEach(el => el.remove());
         let shuffledArr = imgDoubArr.sort((a,b)=> 0.5 - Math.random())
-        console.log('shuffle' + shuffledArr);
-        for (let i =0 ; i < shuffledArr; i++){
-            
+        console.log('before for loop')
+        for (let i =0 ; i < shuffledArr.length; i++){
+            let parentTag = document.getElementById(`${i}`);
+            let child = document.createElement('img');
+            child.src = shuffledArr[i];
+            child.height = '128';
+            parentTag.appendChild(child);
         }
     })
 
